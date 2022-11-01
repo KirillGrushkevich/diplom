1b9a93cb64225ae2db9c32ceb2dae3abab388b33
Kirill Grushkevich <lukedarling@mail.ru>
2022-11-01 11:28:38 +0300

    versio1.0

---
+++ JS/slider-akcia.js
@@ -0,0 +1,15 @@
+const Slider_akcia = new Swiper('.slider-akcia-box', {
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
